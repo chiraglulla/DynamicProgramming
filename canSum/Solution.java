65d773e2334ed06b8fca7fd243c09ac4174ee983
Chirag Lulla <lullachirag239@gmail.coom>
2021-09-08 20:17:59 +0530

    :star:implement tabulation approach

--- canSum/Solution.java
+++ canSum/Solution.java
@@ -1,4 +1,4 @@
-import java.util.HashMap;
+import java.util.*;
 
 public class Solution {
     static boolean canSum(int target, int[] nums) { // brute force
@@ -34,11 +34,31 @@ public class Solution {
         return false;
     }
 
+    static boolean canSumTabulation(int target, int[] nums) {
+        boolean[] table = new boolean[target + 1];
+        Arrays.fill(table, false);
+        table[0] = true;
+
+        for(int i = 0; i <= target; i++) {
+            if(table[i] == true) {
+                for(int j = 0; j < nums.length; j++) {
+                    if(i+nums[j] < table.length)
+                        table[i+nums[j]] = true;
+                }
+            }
+        }
+        return table[target];
+    }
+
     public static void main(String[] args) {
         HashMap<Integer, Boolean> map = new HashMap<Integer, Boolean>();
         int[] arr = {7, 14, 7, 8, 1};
         System.out.println(canSum(7, arr)); 
         System.out.println(canSum(300 , arr));
         System.out.println(canSum(300, arr, map)); 
+        System.out.println(canSumTabulation(7, new int[]{2, 4}));
+        System.out.println(canSumTabulation(300, new int[]{7, 14})); 
+        System.out.println(canSumTabulation(300, arr)); 
+
     }
 }
